002780aa498bbe0b4590f8a79515420efd428e42
Koza, Przemysaw <przemyslaw.koza@cgm.com>
2023-08-04 22:05:35 +0200

    walk, jump animations; speed improvments;

--- src/main.js
+++ src/main.js
@@ -6,24 +6,22 @@
 
 
 import kaboom from "kaboom"
+import "kaboom/global"
 
-const SPEED = 250
+const SPEED = 280
 const JUMP = 800
 const SHAKE = 4
-
+const GRAVITY = 2200
 
 const k = kaboom({ global: false })
-
-
 const soundHit = k.loadSound("hurt", "/sounds/explosion.wav")
 
-//k.camScale(1)
+k.setGravity(GRAVITY)
+k.camScale(1.7)
 k.setBackground("#0c0229")
 
-k.loadSprite("bean", "sprites/bean.png")
 
-
-k.setGravity(2400)
+k.canvas.focus()
 
 const START_POS = [220,30]
 
@@ -36,9 +34,10 @@ k.loadSpriteAtlas("sprites/daxbotsheet_v1.png",{
         sliceX: 4,
         sliceY: 4,
         anims: {
+            idle: 0,
             walk: { from: 0, to: 3 },
             run: { from: 5, to: 7 },
-            jump: { from: 9, to: 11 },
+            jump: { from: 9, to: 11, pingpong: true},
             climb: { from: 12, to: 15 },
         },
     },
@@ -51,57 +50,57 @@ const hero = k.add([
     k.area({scale: [0.5, 0.9]}),
     k.anchor("center"),
     k.body(),
-    "hero"
+    "hero",
+    {
+        dead: false
+    }
 ])
 
 
-k.loadSpriteAtlas("sprites/sci-fi-platformer-tiles-32x32-extension-p1.png",
-                  {
-                      "floor-middle": {
-                          x: 0,
-                          y: 0,
-                          width: 32,
-                          height: 32,
-                          //sliceX: 32,
-                      },
-                      "floor-short": {
-                          x: 32,
-                          y: 0,
-                          width: 32,
-                          height: 32,
-                          //sliceX: 1,
-                      },
-                      "spike-up": {
-                          x: 9 * 32,
-                          y: 4 * 32,
-                          width: 32,
-                          height: 32,
-                      },
-                      "spike-down": {
-                          x: 8 * 32,
-                          y: 4 * 32,
-                          width: 32,
-                          height: 32,
-                      },
-                      "bg-tile1": {
-                          x: 3 * 32,
-                          y: 3 * 32,
-                          width: 32,
-                          height: 32,
-                      },
-                      "grid-vert": {
-                          x: 12 * 32,
-                          y: 4 * 32,
-                          width: 32,
-                          height: 32,
-                      },
-                      "grid-horiz": {
-                          x: 13 * 32,
-                          y: 4 * 32,
-                          width: 32,
-                          height: 32,
-                      },
-                  })
+k.loadSpriteAtlas("sprites/sci-fi-platformer-tiles-32x32-extension-p1.png", {
+    "floor-middle": {
+        x: 0,
+        y: 0,
+        width: 32,
+        height: 32,
+    },
+    "floor-short": {
+        x: 32,
+        y: 0,
+        width: 32,
+        height: 32,
+    },
+    "spike-up": {
+        x: 9 * 32,
+        y: 4 * 32,
+        width: 32,
+        height: 32,
+    },
+    "spike-down": {
+        x: 8 * 32,
+        y: 4 * 32,
+        width: 32,
+        height: 32,
+    },
+    "bg-tile1": {
+        x: 3 * 32,
+        y: 3 * 32,
+        width: 32,
+        height: 32,
+    },
+    "grid-vert": {
+        x: 12 * 32,
+        y: 4 * 32,
+        width: 32,
+        height: 32,
+    },
+    "grid-horiz": {
+        x: 13 * 32,
+        y: 4 * 32,
+        width: 32,
+        height: 32,
+    },
+})
 
 k.scene("main")
 k.addLevel([
@@ -115,10 +114,7 @@ k.addLevel([
     "     i  i             |  i                   i     ",
     "     i  i  ^^  = >    |  i             ^     i    ",
     "================================    =============================================",
-    "",
-    "",
-    "                             ^^^^^^^^^^             ",
-    "                             ==========",
+
 ], {
     // define the size of tile block
     tileWidth: 32,
@@ -135,11 +131,6 @@ k.addLevel([
             k.area(),
             k.body({ isStatic: true }),
         ],
-        // "$": () => [
-        //     k.sprite("bean"), // "coin"
-        //     k.area(),
-        //     k.pos(0, -9),
-        // ],
         "^": () => [
             k.sprite("spike-up"),
             k.area({scale: [0.8, 0.9]}),
@@ -165,7 +156,7 @@ k.addLevel([
         ],
 
     },
-    "wildcardTile?": (symbol) => {
+    wildcardTile: (symbol) => {
         [
             k.sprite("bg-tile1"),
             //k.area()
@@ -179,26 +170,55 @@ function centerCam(pos) {
     console.log(k.camPos())
 }
 
+function centerCamHorizontal(pos) {
+    k.camPos([Math.trunc(hero.pos.x), 110])
+}
+
 
 k.onUpdate(() => {
     if (k.isKeyDown("a")) {
         hero.move(-SPEED, 0)
         hero.flipX = true
+        if (hero.isGrounded() && hero.curAnim() !== "walk") {
+            hero.play("walk")
+        }
     }
     if (k.isKeyDown("d")) {
         hero.move(SPEED, 0)
         hero.flipX = false
+        if (hero.isGrounded() && hero.curAnim() !== "walk") {
+            hero.play("walk")
+        }
     }
-
-    k.camPos([Math.trunc(hero.pos.x), 180])
 })
 
+k.onKeyRelease("d", ()=>{
+    hero.stop()
+})
+k.onKeyRelease("a", ()=>{
+    hero.stop()
+})
 
-//k.onClick(() => k.addKaboom(k.mousePos()))
 
 function jump() {
     if (hero.isGrounded()) {
         hero.jump(JUMP)
+    }
+    hero.play("jump")
+}
+
+function die(){
+    if (!hero.dead) {
+        hero.dead = true
+        k.addKaboom(hero.pos),
+        k.shake(SHAKE)
+        k.play("hurt")
+        k.wait(0.6, () => {
+            hero.moveTo(START_POS)
+            hero.dead = false
+            hero.jump(1)
+            hero.play("jump")
+        })
     }
 }
 
@@ -209,12 +229,22 @@ k.onKeyPress("w", () => {
     jump()
 })
 
-
 hero.onCollide("spike", () => {
-    k.addKaboom(hero.pos),
-    k.shake(SHAKE)
-    k.play("hurt")
-    k.wait(0.5, () => {
-        hero.moveTo(START_POS)
-    })
+    die()
+})
+
+hero.onUpdate(() => {
+    // centerCam(hero.pos)
+    centerCamHorizontal(hero.pos)
+    if (!hero.dead && hero.pos.y > 500) {
+        die()
+    }
+})
+
+k.onKeyPress("f", (c) => {
+    k.setFullscreen(!k.isFullscreen())
+})
+
+hero.onGround(() => {
+    hero.play("idle")
 })
